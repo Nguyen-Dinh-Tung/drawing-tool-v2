ff6f58c682a691757d7ea36b46c64d514768360c
Duc <ducnhse20@gmail.com>
2023-08-06 02:57:31 +0700

    update paint UI

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setModal } from "../../redux/slice/modal.slice";
 import { IconButton } from "@mui/material";
@@ -8,6 +8,7 @@ import { getMeApi } from "../../api/auth.api";
 import { useNotification } from "../../helper/notification";
 function Navbar(props) {
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
   const isLogin = useSelector((state) => state.auth.isLogin);
   const [createNotification] = useNotification();
   const setModalContent = (content) => {
@@ -21,12 +22,16 @@ function Navbar(props) {
     <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" class="flex items-center">
-          <img src="./inkstagram-02.png" class="h-8 mr-3" alt="Final Project" />
+          <img
+            src="./inkstagram.png"
+            class="h-[50px] mr-3"
+            alt="Final Project"
+          />
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Final Project
           </span>
         </Link>
-        <div class="flex md:order-2">
+        <div class="flex gap-5 md:order-2">
           {isLogin ? (
             <IconButton onClick={logout} color="inherit" aria-label="Logout">
               <LogoutIcon />
@@ -38,7 +43,8 @@ function Navbar(props) {
                 onClick={() => {
                   setModalContent("login");
                 }}
-                class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-10 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
+                class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-10 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+              >
                 Sign in
               </button>
               <button
@@ -46,7 +52,8 @@ function Navbar(props) {
                 onClick={() => {
                   setModalContent("register");
                 }}
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
                 Sign up
               </button>
             </>
@@ -56,14 +63,16 @@ function Navbar(props) {
             type="button"
             class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-sticky"
-            aria-expanded="false">
+            aria-expanded="false"
+          >
             <span class="sr-only">Open main menu</span>
             <svg
               class="w-5 h-5"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 17 14">
+              viewBox="0 0 17 14"
+            >
               <path
                 stroke="currentColor"
                 stroke-linecap="round"
@@ -76,20 +85,27 @@ function Navbar(props) {
         </div>
         <div
           class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky">
+          id="navbar-sticky"
+        >
           <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
                 to="/art"
-                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page">
+                className={`block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 ${
+                  pathname === "/art" && "text-blue-700"
+                }`}
+                aria-current="page"
+              >
                 Art
               </Link>
             </li>
             <li>
               <Link
                 to="/paint"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                className={`block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 ${
+                  pathname === "/paint" && "text-blue-700"
+                }`}
+              >
                 Paint
               </Link>
             </li>
@@ -98,7 +114,8 @@ function Navbar(props) {
               <li>
                 <Link
                   to={"/profile"}
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
                   Profile
                 </Link>
               </li>
--- src/pages/paint/Paint.jsx
+++ src/pages/paint/Paint.jsx
@@ -1,184 +1,121 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useState } from "react";
+import FilerobotImageEditor, {
+  TABS,
+  TOOLS,
+} from "react-filerobot-image-editor";
 
-const DrawingComponent = () => {
-  const canvasRef = useRef(null);
-  const contextRef = useRef(null);
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [drawingColor, setDrawingColor] = useState('#000000');
-  const [shape, setShape] = useState('line'); // 'line' or 'rectangle'
-  const [drawings, setDrawings] = useState([]);
-  const currentShape = useRef(null);
-  const [brushSize, setBrushSize] = useState(2);
-  
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    context.lineCap = 'round';
-    context.lineWidth = 2;
-    contextRef.current = context;
-  }, []);
-  
-    useEffect(() => {
-    drawShape()
-  }, [drawings]);
+const ImageEditorComponent = () => {
+  const [imageUrl, setImageUrl] = useState(
+    "/paper.jpg"
+  );
 
-  const startDrawing = (e) => {
-    setIsDrawing(true);
-    const { offsetX, offsetY } = e.nativeEvent;
-    currentShape.current = {
-      shape,
-      color: drawingColor,
-      startX: offsetX,
-      startY: offsetY,
-      brushSize,
-      points: [],
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      setImageUrl(reader.result);
     };
-  };
 
-  const draw = (e) => {
-    if (!isDrawing) return;
-      const { offsetX, offsetY } = e.nativeEvent;
-      if(offsetX || offsetY)
-      if(shape === "line" || shape === "erase"){
-        currentShape.current.points.push({ x: offsetX, y: offsetY });
-      }else if (shape === "rectangle" || shape === "circle"){
-        currentShape.current.points[0] = { x: offsetX, y: offsetY };
-      }
-      drawShape();
-  };
-
-  const stopDrawing = () => {
-    if (isDrawing && currentShape.current) {
-      setIsDrawing(false);
-      setDrawings([...drawings, currentShape.current]);
-      currentShape.current = null;
+    if (file) {
+      reader.readAsDataURL(file);
     }
   };
 
-  const clearCanvas = () => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    setDrawings([]);
+  const handleSaveImage = async (editedImageObject, designState) => {
+    console.log("saved", editedImageObject, designState);
   };
-
-  const undoDrawing = () => {
-    setDrawings((prevDrawings) => prevDrawings.slice(0, -1));
-  };
-
-const drawShape = () => {
-  const canvas = canvasRef.current;
-  const context = canvas.getContext('2d');
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  drawings.forEach((drawing) => {
-    const { shape, color, points, brushSize  } = drawing;
-    context.strokeStyle = color;
-    context.lineWidth = brushSize ;
-    if (shape === 'line' || shape ===  'erase') {
-       if(shape === 'erase'){
-        context.strokeStyle = '#fff';
-      }
-      context.beginPath();
-      context.moveTo(points?.[0]?.x, points?.[0]?.y);
-      points?.forEach((point) => {
-        context.lineTo(point?.x, point?.y);
-      });
-      context.stroke();
-    } else if (shape === 'rectangle') {
-      const { startX, startY } = drawing;
-      if(points?.[0]?.x && points?.[0]?.y){
-      const { x, y } = points?.[0];
-      const width = x - startX;
-      const height = y - startY;
-      context.strokeRect(startX, startY, width, height);
-      }
-    } else if (shape === 'circle') {
-      const { startX, startY } = drawing;
-      if(points?.[0]?.x && points?.[0]?.y){
-      const { x, y } = points?.[0];
-      const radius = Math.sqrt(Math.pow(x - startX, 2) + Math.pow(y - startY, 2));
-      context.beginPath();
-      context.arc(startX, startY, radius, 0, 2 * Math.PI);
-      context.stroke();
-      }
-    }
-  });
-
-  if (currentShape.current) {
-    const { shape, color, points, brushSize } = currentShape.current;
-    context.strokeStyle = color;
-    context.lineWidth = brushSize;
-    if (shape === 'line' || shape ===  'erase') {
-      if(shape === 'erase'){
-        context.strokeStyle = '#fff';
-      }
-      context.beginPath();
-      context.moveTo(points?.[0]?.x, points?.[0]?.y);
-      points.forEach((point) => {
-        context.lineTo(point?.x, point?.y);
-      });
-      context.stroke();
-    } else if (shape === 'rectangle') {
-      const { startX, startY } = currentShape.current;
-      if(points?.[0]?.x && points?.[0]?.y){
-      const { x, y } = points?.[0];
-      const width = x - startX;
-      const height = y - startY;
-      context.strokeRect(startX, startY, width, height);
-      }
-    } else if (shape === 'circle') {
-      const { startX, startY } = currentShape.current;
-      if(points?.[0]?.x && points?.[0]?.y){
-        const { x, y } = points?.[0];
-      const radius = Math.sqrt(Math.pow(x - startX, 2) + Math.pow(y - startY, 2));
-      context.beginPath();
-      context.arc(startX, startY, radius, 0, 2 * Math.PI);
-      context.stroke();
-      }
-    }
-  }
-};
 
   return (
-    <div>
-      <div className='flex gap-5 justify-center py-10' style={{ marginBottom: '10px' }}>
-        <input
-          type="color"
-          value={drawingColor}
-          onChange={(e) => setDrawingColor(e.target.value)}
+    <div className="h-[95vh] bg-[#2B2929]">
+      <div className="h-[85vh] py-5">
+        <div className="px-3 py-5">
+          <label
+            for="fileInput"
+            className="relative cursor-pointer bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-sm text-xs"
+          >
+            Upload File
+            <input
+              id="fileInput"
+              type="file"
+              className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+              onChange={handleFileChange}
+            />
+          </label>
+        </div>
+
+        <FilerobotImageEditor
+          source={imageUrl}
+          theme={{
+            palette: {
+              "bg-primary": "#2B2929",
+              "bg-secondary": "#2B2929",
+            },
+          }}
+          savingPixelRatio={2}
+          previewPixelRatio={2}
+          annotationsCommon={{
+            fill: "#2B2929",
+          }}
+          Text={{
+            text: "...",
+          }}
+          Rotate={{ angle: 90, componentType: "slider" }}
+          Crop={{
+            presetsItems: [
+              {
+                titleKey: "classicTv",
+                descriptionKey: "4:3",
+                ratio: 4 / 3,
+              },
+              {
+                titleKey: "cinemascope",
+                descriptionKey: "21:9",
+                ratio: 21 / 9,
+              },
+            ],
+            presetsFolders: [
+              {
+                titleKey: "socialMedia",
+                groups: [
+                  {
+                    titleKey: "facebook",
+                    items: [
+                      {
+                        titleKey: "profile",
+                        width: 180,
+                        height: 180,
+                        descriptionKey: "fbProfileSize",
+                      },
+                      {
+                        titleKey: "coverPhoto",
+                        width: 820,
+                        height: 312,
+                        descriptionKey: "fbCoverPhotoSize",
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          }}
+          tabsIds={[
+            TABS.ANNOTATE,
+            TABS.ADJUST,
+            TABS.FILTERS,
+            TABS.FINETUNE,
+            TABS.RESIZE,
+          ]}
+          defaultTabId={TABS.ANNOTATE}
+          defaultToolId={TOOLS.PEN}
+          defaultSavedImageQuality={1.0}
+          onSave={(file) =>
+            handleSaveImage(file)
+          }
         />
-        <button onClick={() => setShape('line')}>Draw Line</button>
-        <button onClick={() => setShape('rectangle')}>Draw Rectangle</button>
-        <button onClick={() => setShape('circle')}>Draw circle</button>
-        <button onClick={() => { setShape('erase') }}>Erase</button>
-       <div className='flex gap-2'>
-        Size: 
-         <input
-          type="range"
-          min="1"
-          max="10"
-          value={brushSize}
-          onChange={(e) => setBrushSize(Number(e.target.value))}
-      />
-        <span>{brushSize}</span>
-       </div>
-        <button onClick={clearCanvas}>Clear</button>
-        <button onClick={undoDrawing}>Undo</button>
       </div>
-      <canvas
-        className='mx-auto'
-        ref={canvasRef}
-        onMouseDown={startDrawing}
-        onMouseMove={draw}
-        onMouseUp={stopDrawing}
-        onMouseLeave={stopDrawing}
-        width={1000}
-        height={500}
-        style={{ border: '1px solid black' }}
-      />
     </div>
   );
 };
 
-export default DrawingComponent;
+export default ImageEditorComponent;
